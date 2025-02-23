c3291052b2dc159754220421d2ca9f42ea868078
ramudi48 <ramudimunasinghe2002@gmail.com>
2025-02-23 21:04:47 +0530

    update homepage.php

--- homepage.php
+++ homepage.php
@@ -11,8 +11,19 @@ include("connect.php");
     <title>Homepage</title>
 </head>
 <body>
-<div style="text-align:center; padding:15%;">
-     
+    <div style="text-align:center; padding:15%;">
+      <p  style="font-size:50px; font-weight:bold;">
+       Hello  <?php 
+       if(isset($_SESSION['email'])){
+        $email=$_SESSION['email'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'].' '.$row['lastName'];
+        }
+       }
+       ?>
+       :)
+      </p>
       <a href="logout.php">Logout</a>
     </div>
 </body>
